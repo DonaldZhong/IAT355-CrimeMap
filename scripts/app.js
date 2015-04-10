783204de7b69ecbf7ea034122f2663b2c0849665
DonaldZhong <donaldz@sfu.ca>
2015-04-09 21:46:03 -0700

    UI changes and line page addition

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var visApp = angular.module('visApp', ['ngRoute','ngMap']);
+var visApp = angular.module('visApp', ['ngRoute', 'ngMaterial','ngMap']);
 
     // configure our routes
     visApp.config(function($routeProvider) {
@@ -10,12 +10,43 @@ var visApp = angular.module('visApp', ['ngRoute','ngMap']);
             .when('/', {
                 templateUrl : 'pages/main.html',
                 controller  : 'mainController'
+            })
+            .when('/line', {
+                templateUrl : 'pages/line.html',
+                controller  : 'lineController'
             });
     });
 
     // create the controller and inject Angular's $scope
-    visApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
+    visApp.controller('mainController', function($scope, $timeout, $mdSidenav, $log) {
+        $scope.toggleRight = function() {
+        $mdSidenav('right').toggle()
+            .then(function(){
+                $log.debug("toggle RIGHT is done");
+            });
+          }; 
+
+        console.log("on the Home Page")
+
     });
 
+    visApp.config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue-grey')
+            .accentPalette('amber');
+    });
+
+    visApp.controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function() {
+        $mdSidenav('right').close()
+            .then(function(){
+                $log.debug("close RIGHT is done");
+        });
+      };
+    });
+
+    visApp.controller('lineController', function($scope) {
+            console.log("on the line graph page");
+            $scope.message = 'test message please ignore';
+   
+    });
